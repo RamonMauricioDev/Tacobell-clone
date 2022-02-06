b5b89468a814e1d8fc10341fac3480b15d54ea9b
Argo Tenacius <ArgoTenacius@gmail.com>
2022-02-06 11:57:12 -0300

    Classes name added to the navbar items.

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -4,15 +4,15 @@ import './navbar.css'
 
 const navbar = () => {
   return (
-    <div className='navbar'>
+    <div className='Navbar__div'>
       <img src={Logo} alt='tacobell-logo'/>
-      <ul>
-        <li><a>OUR FOOD</a></li>
-        <li><a>FIND US</a></li>
-        <li><a>NUTRITION</a></li>
-        <li><a>ABOUT US</a></li>
-        <li><a>WHAT'S NEW</a></li>
-        <li><a>MORE+</a></li>
+      <ul className='Navbar__list'>
+        <li className='Navbar__item'><a>OUR FOOD</a></li>
+        <li className='Navbar__item'><a>FIND US</a></li>
+        <li className='Navbar__item'><a>NUTRITION</a></li>
+        <li className='Navbar__item'><a>ABOUT US</a></li>
+        <li className='Navbar__item'><a>WHAT'S NEW</a></li>
+        <li className='Navbar__item'><a>MORE+</a></li>
       </ul>
     </div>
     )
