cc2d6ed5b8f501cf06b9e277a08f88aeefd6884a
Argo Tenacius <ArgoTenacius@gmail.com>
2022-02-10 17:34:52 -0300

    Our locations base done.

--- src/containers/ourlocations/Ourlocations.jsx
+++ src/containers/ourlocations/Ourlocations.jsx
@@ -5,6 +5,11 @@ import Delhi from './ourlocations images/Delhi.jpg';
 const Ourlocations = () => {
   return (
     <div className='Ourlocations__container'>
+        <p className='Location__info'>
+            <h4>Our Restaurants</h4>
+            <h2>Connaught Place, Delhi</h2>
+        </p>
+        <a href="#">Click here to see 360deg</a>
     </div>
   )
 }
