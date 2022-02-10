53472b640e048191aefecb346f160d90782e6d91
Argo Tenacius <ArgoTenacius@gmail.com>
2022-02-09 22:06:38 -0300

    Menu grid base done.

--- src/containers/ourmenu/Ourmenu.jsx
+++ src/containers/ourmenu/Ourmenu.jsx
@@ -1,11 +1,43 @@
 import React from 'react'
 import './ourmenu.css'
+import Burrito from './ourmenu images/burrito.webp';
+import Best_of_bell from './ourmenu images/best_of_bell.webp';
+import Meals from './ourmenu images/meals.webp';
+import Quesadilha from './ourmenu images/quesadilha.webp';
+import Specialties from './ourmenu images/specialties.webp';
+import Taco from './ourmenu images/taco.webp';
 
 const Ourmenu = () => {
   return (
     <div className='ourmenu__div'>
       <h4>Our menu</h4>
       <h3>The line-up</h3>
+      <section class="menu__grid">
+        <div className='menu__container'>
+          <img src={Taco} alt="" className='image__responsive'/>
+          <p>Taco</p>
+        </div>
+        <div className="menu__container">
+          <img src={Burrito} alt="" className='image__responsive'/>
+          <p>Burrito</p>
+        </div>
+        <div className="menu__container">
+          <img src={Best_of_bell} alt="" className='image__responsive'/>
+          <p>Best of bell</p>
+        </div>
+        <div className="menu__container">
+          <img src={Specialties} alt="" className='image__responsive'/>
+          <p>Specialities</p>
+        </div>
+        <div className="menu__container">
+          <img src={Quesadilha} alt="" className='image__responsive'/>
+          <p>Quesadilha</p>
+        </div>
+        <div className="menu__container">
+          <img src={Meals} alt="" className='image__responsive'/>
+          <p>Meals</p>
+        </div>
+      </section>
     </div>
   )
 }
