dd02514850e55997c114697f226382584009452e
Argo Tenacius <ArgoTenacius@gmail.com>
2022-02-12 12:16:54 -0300

    Taconews__grid changed to Taconews__grid-right and applied neccesary changes.

--- src/containers/taconews/Taconews.jsx
+++ src/containers/taconews/Taconews.jsx
@@ -8,7 +8,19 @@ const Taconews = () => {
         <h4>Our news</h4>
         <h2>Taco news</h2>
       </header>
-      <div className='taconews__grid'>
+      <div className='taconews__grid-right'>
+        <div className='taco__news'>
+          <div className='taconews__photo'></div>
+          <div className='taconews__info'>
+            <h6 className="taconews__date">nov 02, 2021</h6>
+            <a href="#">
+              <h4 className="taconews__title">
+                Tacobell goes a step ahead, throws an unique
+                cheesyween party for it's customers on hallowen
+              </h4>
+            </a>
+          </div>
+        </div>
         <div className='taco__news'>
           <div className='taconews__photo'></div>
           <div className='taconews__info'>
