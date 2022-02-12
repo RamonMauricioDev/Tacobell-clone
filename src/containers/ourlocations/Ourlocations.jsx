2c6812723db05b226229e9fded7ee5356b1e399a
Argo Tenacius <ArgoTenacius@gmail.com>
2022-02-12 10:21:41 -0300

    Our menu structure and appearance complete.

--- src/containers/ourlocations/Ourlocations.jsx
+++ src/containers/ourlocations/Ourlocations.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import './ourlocations.css'
-import Delhi from './ourlocations images/Delhi.jpg';
+import deg from './ourlocations images/360-degrees.webp'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleRight, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 
 const Ourlocations = () => {
   return (
-    <div className='Ourlocations__container'>
-        <p className='Location__info'>
+    <div className='ourlocation__container'>
+      <div className='ourlocation__content'>
+          <div className='ourlocation__info'>
             <h4>Our Restaurants</h4>
-            <h2>Connaught Place, Delhi</h2>
-        </p>
-        <a href="#">Click here to see 360deg</a>
+            <h2>CONNAUGHT PLACE, DELHI</h2>
+          </div>
+          <div className='ourlocation__interaction'>
+            <div className='ourlocation__buttons'>
+              <button><FontAwesomeIcon icon={faAngleLeft} color="#fff" size='2x'/></button>
+              <button><FontAwesomeIcon icon={faAngleRight} color="#fff" size='2x'/></button>
+            </div>
+            <div>
+              <a href="" className='ourlocation__360'>
+                  <p>Click here to see</p>
+                  <img src={deg}></img>
+              </a>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
