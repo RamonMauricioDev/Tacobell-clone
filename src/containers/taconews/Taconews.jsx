fa646d3b8b4fbbb2123da7d5129e8eb591e159c1
Argo Tenacius <ArgoTenacius@gmail.com>
2022-02-13 09:54:43 -0300

    Responsive news added, still need to implement to it update every time the window changes size.

--- src/containers/taconews/Taconews.jsx
+++ src/containers/taconews/Taconews.jsx
@@ -1,7 +1,110 @@
 import React from 'react'
 import './taconews.css'
 
+const ResponsiveNews = () => (
+  <>
+          <div className='taco__news'>
+            <div className='taconews__photo'></div>
+            <div className='taconews__info'>
+              <h6 className="taconews__date">nov 02, 2021</h6>
+              <a href="#">
+                <h4 className="taconews__title">
+                  Tacobell goes a step ahead, throws an unique
+                  cheesyween party for it's customers on hallowen
+                </h4>
+              </a>
+            </div>
+          </div>
+
+
+            <div className='taco__news'>
+              <div className='taconews__photo'></div>
+              <div className='taconews__info'>
+                <h6 className="taconews__date">dec 02, 2021</h6>
+                <a href="#">
+                  <h4 className="taconews__title">Tacobell offers
+                  a cheesy surprise for its consumers with the launch of the ultimate cheese taco</h4>
+                </a>
+              </div>
+            </div>
+  </>
+)
+
+const Fullnews = () => (
+  <>
+  <div className='taco__news principal'>
+            <div className='spotlight'></div>
+            <div className='taconews__info'>
+              <h6 className="taconews__date">nov 02, 2021</h6>
+              <a href="#">
+                <h4 className="taconews__title">
+                  Tacobell goes a step ahead, throws an unique
+                  cheesyween party for it's customers on hallowen
+                </h4>
+              </a>
+              <p className='taconews__description'>
+                  Taco Bell, the world’s leading Mexican-inspired restaurant brand, brings another delightful cheesy surprise for its consumers after the Grilled Cheese Burrito & Quesadilla, with the launch of The Ultimate Cheese Taco.
+                  <br />
+                  <br />
+                  Filled with the goodness of cheese, The Ultimate Cheese Taco is sure to satisfy all your cheesy cravings. A cheese lover’s delight.
+                </p>
+            </div>
+          </div>
+
+
+            <div className='taco__news'>
+              <div className='taconews__photo'></div>
+              <div className='taconews__info'>
+                <h6 className="taconews__date">dec 02, 2021</h6>
+                <a href="#">
+                  <h4 className="taconews__title">Tacobell offers
+                  a cheesy surprise for its consumers with the launch of the ultimate cheese taco</h4>
+                </a>
+              </div>
+            </div>
+
+    <div className='taco__news'>
+      <div className='taconews__photo'></div>
+      <div className='taconews__info'>
+        <h6 className="taconews__date">oct 04, 2021</h6>
+        <a href="#">
+          <h4 className="taconews__title">
+            Taco Bell hits the ultimate six this cricketing season with See A Six, Catch a Taco 
+          </h4>
+        </a>
+      </div>
+    </div>
+
+
+    <div className='taco__news'>
+      <div className='taconews__photo'></div>
+      <div className='taconews__info'>
+        <h6 className="taconews__date">oct 1, 2021</h6>
+        <a href="#">
+          <h4 className="taconews__title">
+            Taco Bell introduces Grilled Cheese Burrito & Quesadilla
+          </h4>
+        </a>
+      </div>
+    </div>
+
+
+    <div className='taco__news'>
+      <div className='taconews__photo'></div>
+      <div className='taconews__info'>
+        <h6 className="taconews__date">aug 05, 2021</h6>
+        <a href="#">
+          <h4 className="taconews__title">
+            Taco Bell delights fans, introduces new 'Taco Party Feast' 
+          </h4>
+        </a>
+      </div>
+    </div>
+  </>
+)
+
 const Taconews = () => {
+
   return (
     <div className='taconews__container'>
       <header className='taconews__header'>
@@ -9,84 +112,14 @@ const Taconews = () => {
         <h3>Taco news</h3>
       </header>
       <div className='taconews__grid'>
-
-      <div className='taco__news principal'>
-          <div className='spotlight'></div>
-          <div className='taconews__info'>
-            <h6 className="taconews__date">nov 02, 2021</h6>
-            <a href="#">
-              <h4 className="taconews__title">
-                Tacobell goes a step ahead, throws an unique
-                cheesyween party for it's customers on hallowen
-              </h4>
-            </a>
-            <p className='taconews__description'>
-                Taco Bell, the world’s leading Mexican-inspired restaurant brand, brings another delightful cheesy surprise for its consumers after the Grilled Cheese Burrito & Quesadilla, with the launch of The Ultimate Cheese Taco.
-                <br />
-                <br />
-                Filled with the goodness of cheese, The Ultimate Cheese Taco is sure to satisfy all your cheesy cravings. A cheese lover’s delight.
-              </p>
-          </div>
+            {
+              document.documentElement.clientWidth > 950 ?
+                <Fullnews />
+              :
+                <ResponsiveNews />
+            }
         </div>
-        {/*
-        Up-left
-        */}
-        <div className='taco__news'>
-          <div className='taconews__photo'></div>
-          <div className='taconews__info'>
-            <h6 className="taconews__date">dec 02, 2021</h6>
-            <a href="#">
-              <h4 className="taconews__title">Tacobell offers
-              a cheesy surprise for its consumers with the launch of the ultimate cheese taco</h4>
-            </a>
-          </div>
-        </div>
-        
-        {/*
-        Up-right      
-        */}
-        <div className='taco__news'>
-          <div className='taconews__photo'></div>
-          <div className='taconews__info'>
-            <h6 className="taconews__date">oct 04, 2021</h6>
-            <a href="#">
-              <h4 className="taconews__title">
-                Taco Bell hits the ultimate six this cricketing season with See A Six, Catch a Taco 
-              </h4>
-            </a>
-          </div>
-        </div>
-        {/*
-        Down-left   
-        */}
-        <div className='taco__news'>
-          <div className='taconews__photo'></div>
-          <div className='taconews__info'>
-            <h6 className="taconews__date">oct 1, 2021</h6>
-            <a href="#">
-              <h4 className="taconews__title">
-                Taco Bell introduces Grilled Cheese Burrito & Quesadilla
-              </h4>
-            </a>
-          </div>
-        </div>
-        {/*
-        Down-right
-        */}
-        <div className='taco__news'>
-          <div className='taconews__photo'></div>
-          <div className='taconews__info'>
-            <h6 className="taconews__date">aug 05, 2021</h6>
-            <a href="#">
-              <h4 className="taconews__title">
-                Taco Bell delights fans, introduces new 'Taco Party Feast' 
-              </h4>
-            </a>
-          </div>
-        </div>
-
       </div>
-    </div>
   )
 }
 
