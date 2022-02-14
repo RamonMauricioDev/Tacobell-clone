ca47a4b107e744ac312f5d2b9964ff7d358c7044
Argo Tenacius <ArgoTenacius@gmail.com>
2022-02-14 18:19:27 -0300

    Added the description.

--- src/containers/taconews/Taconews.jsx
+++ src/containers/taconews/Taconews.jsx
@@ -16,7 +16,11 @@ const Fullnews = () => (
       <div className='taconews__info full'>
         <h4 className='taconews__date'>dec 02, 2021</h4>
         <a href="#" className='taconews__title'>Tacobell offers a cheesy surprise for its consumers with the launch of the ultimate cheese taco</a>
-        
+        <p className="taconews__description">
+        Taco Bell, the world’s leading Mexican-inspired restaurant brand, brings another delightful cheesy surprise for its consumers after the Grilled Cheese Burrito & Quesadilla, with the launch of The Ultimate Cheese Taco.
+        <br /> <br />
+        Filled with the goodness of cheese, The Ultimate Cheese Taco is sure to satisfy all your cheesy cravings. A cheese lover’s delight.
+        </p>
       </div>
     </div>
   </>
